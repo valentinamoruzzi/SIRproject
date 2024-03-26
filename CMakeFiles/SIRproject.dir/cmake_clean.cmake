bf25becab2b20d6d2ac48c5ab4541a141b6b1e40
valentinamoruzzi <163642225+valentinamoruzzi@users.noreply.github.com>
2024-03-26 15:57:44 +0100

    Cercando di sistemRE SIRMODEL

---
+++ CMakeFiles/SIRproject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SIRproject.dir/SIRproject.cpp.o"
+  "CMakeFiles/SIRproject.dir/SIRproject.cpp.o.d"
+  "CMakeFiles/SIRproject.dir/src/sirdata.cpp.o"
+  "CMakeFiles/SIRproject.dir/src/sirdata.cpp.o.d"
+  "CMakeFiles/SIRproject.dir/src/sirmodel.cpp.o"
+  "CMakeFiles/SIRproject.dir/src/sirmodel.cpp.o.d"
+  "SIRproject"
+  "SIRproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SIRproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/sir.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sir.dir/SIRproject.cpp.o"
+  "CMakeFiles/sir.dir/SIRproject.cpp.o.d"
+  "CMakeFiles/sir.dir/src/sir_model.cpp.o"
+  "CMakeFiles/sir.dir/src/sir_model.cpp.o.d"
+  "CMakeFiles/sir.dir/src/sirdata.cpp.o"
+  "CMakeFiles/sir.dir/src/sirdata.cpp.o.d"
+  "sir"
+  "sir.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sir.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
