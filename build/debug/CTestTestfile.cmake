790a3c25bed6ae1ccaca7c9a9c561d48c00b6873
valentinamoruzzi <163642225+valentinamoruzzi@users.noreply.github.com>
2024-05-08 11:10:01 +0200

    correzioni

--- build/debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/valentinamoruzzi/Documents/GitHub/SIRproject
-# Build directory: /Users/valentinamoruzzi/Documents/GitHub/SIRproject/build/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/build/release/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/valentinamoruzzi/Documents/GitHub/SIRproject/src
-# Build directory: /Users/valentinamoruzzi/Documents/GitHub/SIRproject/src/build/release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
